9c5527a2b34de116fc1b547db03bb70bb74b2375
Nicolas garcia <nicolasg99dr@gmail.com>
2023-07-30 00:04:56 -0300

    add controllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,79 @@
+const userSchema = require('../models/userSchema');
+
+// Controlador para registrar un nuevo usuario
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email, birthdate, country, premium, followers, playlists } = req.body;
+    const newUser = new userSchema({ name, email, birthdate, country, premium, followers, playlists});
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Controlador para obtener un usuario por su ID
+exports.getUserById = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const user = await userSchema.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado.' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Controlador para obtener todos los usuarios registrados
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await userSchema.find();
+
+    if (!users || users.length === 0) {
+      return res.status(404).json({ message: 'No se encontraron usuarios.' });
+    }
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Controlador para eliminar un usuario por su ID
+exports.deleteUserById = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const existingUser = await userSchema.findById(userId);
+
+    if (!existingUser) {
+      return res.status(404).json({ message: 'No existe este usuario: ' + userId });
+    }
+    await userSchema.deleteOne({ _id: userId });
+    res.json({ message: 'Usuario eliminado correctamente: ' + userId });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Controlador para editar datos de un usuario ya creado
+exports.updateUserById = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const existingUser = await userSchema.findById(userId);
+
+    if (!existingUser) {
+      return res.status(404).json({ message: 'No existe este usuario.' });
+    }
+
+    existingUser.set(req.body);
+    const updatedUser = await existingUser.save();
+
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,89 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const userSchema = require('../models/userSchema');
+const userController = require('../controllers/userController');
 
-// Ruta POST para registrar un nuevo usuario
-router.post('/users', async (req, res) => {
-  try {
-    const { name, email, birthdate, country, premium, followers, playlists } = req.body;
-    // Crea una nueva instancia del modelo userSchema con los datos recibidos en el cuerpo de la solicitud
-    const newUser = new userSchema({ name, email, birthdate, country, premium, followers, playlists});
-    // Guarda el nuevo usuario en la base de datos
-    const savedUser = await newUser.save();
-    res.json(savedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-
-//solicitar informacion de un usuario especifico.
-router.get('/users/:userId', async (req,res) =>{
-  try{
-    const userId= req.params.userId;
-    const user = await userSchema.findById(userId);
-    if(!user){
-     return res.status(404).json({message: 'Usuario no encontrado.'})
-    }
-    res.json(user);
-  }catch(error){
-    res.status(500).json({message: error.message})
-  }
-})
-
-// solicitar todos los usuarios registrados.
-router.get('/users', async (req, res) => {
-  try {
-    const users = await userSchema.find();
-
-    if (!users || users.length === 0) {
-      return res.status(404).json({ message: 'No se encontraron usuarios.' });
-    }
-
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//eliminar un usuario por id.
-router.delete('/users/:userId', async (req,res) =>{
-  try{
-    const userId= req.params.userId;
-    const existingUser = await userSchema.findById(userId);
-
-    if(!existingUser){
-      return res.status(404).json({message: 'No existe este usuario: '+userId});
-    }
-    await userSchema.deleteOne({ _id: userId });
-    res.json({message: 'Usuario eliminado correctamente: '+userId})
-  }catch(error){
-   res.status(500).json({message: error.message});
-  }
-})
-
-//editar datos de un usuario ya creado.
-
-router.put('/users/:userId', async (req, res) => {
-  try {
-    const userId = req.params.userId;
-
-    // Verificar si el usuario existe en la base de datos
-    const existingUser = await userSchema.findById(userId);
-    if (!existingUser) {
-      return res.status(404).json({ message: 'No existe este usuario.' });
-    }
-
-    // Actualizar los datos del usuario con los nuevos datos del cuerpo de la solicitud
-    existingUser.set(req.body);
-    
-    // Guardar los cambios en la base de datos
-    const updatedUser = await existingUser.save();
-
-    res.json(updatedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.post('/users', userController.createUser);
+router.get('/users/:userId', userController.getUserById);
+router.get('/users', userController.getAllUsers);
+router.delete('/users/:userId', userController.deleteUserById);
+router.put('/users/:userId', userController.updateUserById);
 
 module.exports = router;
